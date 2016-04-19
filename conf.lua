5bea35fc86029c5b93c3e3b5726cae30c5395e67
bjorn <tie.372@gmail.com>
2016-04-18 22:31:36 -0700

    Fullscreen is weird;

--- conf.lua
+++ conf.lua
@@ -3,10 +3,5 @@ function love.conf(t)
   t.window.title = 'Quadra'
   t.window.icon = 'art/icon.png'
   t.window.highdpi = false
-  t.window.width = 0
-  t.window.height = 0
-  t.window.fullscreen = true
-  t.window.fullscreentype = 'exclusive'
-  t.window.resizable = true
   t.modules.physics = false
 end
