dce084f86d2e9ba0d7f19a94951fe38f52dd9b0b
bjorn <tie.372@gmail.com>
2016-04-18 23:13:21 -0700

    Highdpi;

--- conf.lua
+++ conf.lua
@@ -2,6 +2,6 @@ function love.conf(t)
   t.identity = 'Quadra'
   t.window.title = 'Quadra'
   t.window.icon = 'art/icon.png'
-  t.window.highdpi = false
+  t.window.highdpi = true
   t.modules.physics = false
 end
